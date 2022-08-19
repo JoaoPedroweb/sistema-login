48267acf260696edd9bad5f23f96b151f1d7a40e
JoaoPedroweb <joaopfdossanto@gmail.com>
2022-08-18 22:09:48 -0300

    sistema login

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Signin from "../pages/Home/Signin";
+import Signup from "../pages/Home/Signup";
+
+const Private = ({ Item }) => {
+ const signed = useAuth();
+
+ return signed > 0 ? <Item /> : <signin />;
+
+};
+
+
+const RoutesApp = () => {
+  return (
+  <BrowserRouter>
+    <Fragment>
+       <Routes>
+        <Route exact path="/home" element={<Private Item={Home} />} />
+        <Route path="/" element={<Signin />} />
+        <Route path="*" element={<Signup />} />
+       </Routes>
+    </Fragment>
+  </BrowserRouter>
+
+
+  );
+
+};
+
+export default RoutesApp;
